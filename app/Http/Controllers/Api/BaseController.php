420df6816a1673012fecd28236f21de19ee1339e
santani423 <santani423@gmail.com>
2026-03-25 16:39:22 +0700

    base

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Paket;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
@@ -63,53 +64,10 @@ class BaseController extends Controller
 
     public function paket()
     {
+        $paket = Paket::all();
         $data = [
             'message' => 'Demo API works',
-            'demos' => [
-                'website' => [
-                    [
-                        'title' => 'Wedding Theme',
-                        'description' => 'Pastel gold elegant',
-                        'category' => 'Pernikahan',
-                        'thumbnail' => '/placeholder-wedding.jpg'
-                    ],
-                    [
-                        'title' => 'Khitanan Theme',
-                        'description' => 'Hijau islami',
-                        'category' => 'Khitanan',
-                        'thumbnail' => '/placeholder-khitanan.jpg'
-                    ],
-                    [
-                        'title' => 'Birthday Theme',
-                        'description' => 'Ceria balloons',
-                        'category' => 'Ulang Tahun',
-                        'thumbnail' => '/placeholder-birthday.jpg'
-                    ]
-                ],
-                'video' => [
-                    [
-                        'title' => 'Wedding Video Invitation',
-                        'description' => 'Animated Gold Theme',
-                        'category' => 'Pernikahan',
-                        'thumbnail' => '/placeholder-wedding-video.jpg',
-                        'videoUrl' => '#'
-                    ],
-                    [
-                        'title' => 'Khitanan Video Invitation',
-                        'description' => 'Islamic Green Theme',
-                        'category' => 'Khitanan',
-                        'thumbnail' => '/placeholder-khitanan-video.jpg',
-                        'videoUrl' => '#'
-                    ],
-                    [
-                        'title' => 'Birthday Video Invitation',
-                        'description' => 'Colorful Celebration',
-                        'category' => 'Ulang Tahun',
-                        'thumbnail' => '/placeholder-birthday-video.jpg',
-                        'videoUrl' => '#'
-                    ]
-                ]
-            ]
+            'paket'   => $paket,   
         ];
 
         return response()->json($data);
