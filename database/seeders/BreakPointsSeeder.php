d9b5f653e9e412c66918f2b6bc8af4dbb3129df9
santani423 <santani423@gmail.com>
2026-05-05 15:09:06 +0700

    oke

--- database/seeders/BreakPointsSeeder.php
+++ database/seeders/BreakPointsSeeder.php
@@ -10,14 +10,14 @@ class BreakPointsSeeder extends Seeder
     public function run(): void
     {
         $breakpoints = [
-            ['name' => 'xxs',    'code' => 'xxs',    'size' => '360px'],
-            ['name' => 'xs',     'code' => 'xs',     'size' => '375px'],
-            ['name' => 's',      'code' => 's',      'size' => '390px'],
-            ['name' => 'iphone', 'code' => 'iphone', 'size' => '412px'],
-            ['name' => 'md',     'code' => 'md',     'size' => '768px'],
-            ['name' => 'md2',    'code' => 'md2',    'size' => '810px'],
-            ['name' => 'md3',    'code' => 'md3',    'size' => '850px'],
-            ['name' => 'tb',     'code' => 'tb',     'size' => '900px'],
+            ['name' => 'xxs',    'code' => 'xxs',    'sekala' => '360px'],
+            ['name' => 'xs',     'code' => 'xs',     'sekala' => '375px'],
+            ['name' => 's',      'code' => 's',      'sekala' => '390px'],
+            ['name' => 'iphone', 'code' => 'iphone', 'sekala' => '412px'],
+            ['name' => 'md',     'code' => 'md',     'sekala' => '768px'],
+            ['name' => 'md2',    'code' => 'md2',    'sekala' => '810px'],
+            ['name' => 'md3',    'code' => 'md3',    'sekala' => '850px'],
+            ['name' => 'tb',     'code' => 'tb',     'sekala' => '900px'],
         ];
 
         DB::table('breack_poins')->insert($breakpoints);
