c2474b5615a1b09320d33e93240b4816bd72678b
santani423 <santani423@gmail.com>
2026-05-05 15:56:22 +0700

    oke

--- app/Models/Tema.php
+++ app/Models/Tema.php
@@ -8,6 +8,6 @@ class Tema extends Model
 {
     public function asset()
     {
-        return $this->belongsTo(Asset::class, 'id', 'tema_id');
+        return $this->hasMany(Asset::class, 'id', 'tema_id');
     }
 }
