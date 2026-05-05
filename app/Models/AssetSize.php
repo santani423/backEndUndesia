032605751c33733dff9667e62adb5158addce85b
santani423 <santani423@gmail.com>
2026-05-05 16:07:26 +0700

    oke

--- app/Models/AssetSize.php
+++ app/Models/AssetSize.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssetSize extends Model
 {
-    //
+    public function breakpoint()
+    {
+        return $this->belongsTo(BreackPoin::class, 'breack_poin_id');
+    }
+
+    public function sizeTema()
+    {
+        return $this->belongsTo(SizeTema::class, 'size_tema_id');
+    }
 }
