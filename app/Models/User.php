4afd45425a757d541779ee0b5e5728e03d0e3c9d
santani423 <santani423@gmail.com>
2026-05-18 16:55:02 +0700

    domain

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Quote::class, 'id_user');
     }
+
+    public function data()
+    {
+        return $this->hasOne(Data::class, 'id_user');
+    }
 }
