ad79bf19c1c3cc35db058d721c81cd7d6a8f4e0c
santani423 <santani423@gmail.com>
2026-05-28 10:51:27 +0700

    oke

--- app/Models/Tamu.php
+++ app/Models/Tamu.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tamu extends Model
 {
-    protected $table = 'tamus';
+    protected $table = 'tamu';
     protected $primaryKey = 'id_tamu';
     public $timestamps = false;
     protected $fillable = ['nama_tamu', 'nama_slug', 'alamat_tamu', 'alamat_slug', 'no_wa', 'qrcode', 'id_user', 'tgl_kirim', 'status_kirim', 'status', 'waktu_hadir'];
