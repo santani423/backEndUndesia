5f8f10c3dbd6f97a1ba932d679c5d8a1536b004a
santani423 <santani423@gmail.com>
2026-05-28 12:19:37 +0700

    oke

---
+++ database/migrations/2026_05_28_051755_create_theme_colors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('theme_colors', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('tema_id')->nullable();
+             $table->string('key')->unique();
+            $table->string('value', 50);
+
+            $table->string('label')->nullable();
+            $table->string('group')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('theme_colors');
+    }
+};
